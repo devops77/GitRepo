65ed9f1b672aa7051fda05251e7c8fd2686ea40d
Tom Devlin <Tom.H.Devlin@wdc.com>
2018-03-30 14:16:13 -0700

    DS exp

---
+++ DeathStar2018/DeathStarExplode.cpp
@@ -0,0 +1,73 @@
+#define DEBUG
+#include "debugArduino.h"
+#include "global.h"  //global defines
+#include "DeathStarExplodeExplode.h"
+
+
+
+
+	void DeathStarExplode::initAttributes ()
+	{
+		
+	}	
+
+
+	 /**
+	   * Empty Constructor
+	   */
+
+	 DeathStarExplode ();
+
+
+
+
+
+
+  void DeathStarExplode::linkVentPort(VentPort* newPort)
+  {
+  
+  }
+
+
+  /**
+   * init the vars when this script starts
+   * set step = 0
+   * updat = now
+   */
+  virtual void DeathStarExplode::startSceen ()
+  {
+	  
+  }
+  /**
+   * think this will be a big switch based on step value
+   */
+  virtual void DeathStarExplode::run ()
+  {
+	  
+  }
+
+
+
+
+// steps to get this done
+  void DeathStarExplode::doRingThrob()
+  {
+	  
+  }
+  
+  
+  void doDeathStarSpread()
+  {
+	  
+  }
+  
+  void doFlash()
+  {
+	  
+  }
+  
+  void DeathStarExplode::doFadeToBlack()
+  {
+	  
+  }
+  
